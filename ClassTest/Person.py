d582e535dba4dc391b4f21739aed2b4e90cd8540
马俭 <majian@hxrainbow.com>
2018-08-25 16:51:12 +0800

    [主工程] 修改 修改了目录结构

--- ClassTest/Person.py
+++ ClassTest/Person.py
@@ -1,3 +1,6 @@
+# 创建父类，继承object
+#__init__为构造函数
+# __del__为析构函数
 class Person(object):
     def __init__(self,name,age):
         self.name = name
@@ -9,6 +12,10 @@ class Person(object):
     def get_age(self):
         return self.age
 
+    def get_type(self):
+        return '我是老师'
+
+# 继承Person类
 class Teacher(Person):
     def __init__(self, name, age, number):
         super(Teacher, self).__init__(name, age)
@@ -16,6 +23,9 @@ class Teacher(Person):
 
     def get_number(self):
         return  self.number
+
+    def get_type(self):
+        return '我是学生'
 
 
 
