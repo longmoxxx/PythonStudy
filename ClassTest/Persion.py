e5f47723de42e3b74fa4cb15a82468bbeb83156b
马俭 <majian@hxrainbow.com>
2018-08-25 16:13:23 +0800

    [主工程] 修改 添加了忽略文件

--- ClassTest/Persion.py
+++ ClassTest/Persion.py
@@ -4,14 +4,14 @@ class Person(object):
         self.age = age
 
     def get_name(self):
-        return  self.name
+        return self.name
 
     def get_age(self):
         return self.age
 
 class Teacher(Person):
-    def __init__(self,name,age,number):
-        super(Teacher,self).__init__(name,age)
+    def __init__(self, name, age, number):
+        super(Teacher, self).__init__(name, age)
         self.number = number
 
     def get_number(self):
